28fba6962da3d1b4590bf44ccee00f47f2e83d7a
Samirbelabed <lesam88@hotmail.fr>
2024-12-05 15:16:26 +0000

    first commit

---
+++ js/euromillions.js
@@ -0,0 +1,121 @@
+/* L'euromillions consiste à faire un tirage de 5 nombres entre 1 et 50 et deux numéros étoiles entre 1 et 12
+
+liste de courses bien utiles :
+Math.random()
+while(**)
+Math.ceil(**)
+push(**)
+includes(**)
+setTimeout(**)
+
+*/
+
+
+let euro = document.getElementById('euromillions');
+let res = document.getElementById('resultat');
+let bshow = document.getElementsByClassName('zone boule show');
+let eshow = document.getElementsByClassName('zone etoile show');
+
+
+const numeros = [];
+const compl = [];
+
+
+while (numeros.length < 5) {
+
+
+    let nombreAleatoire = Math.floor(Math.random() * 50) + 1;
+    console.log(nombreAleatoire);
+
+    if (!numeros.includes(nombreAleatoire)) {
+
+        numeros.push(nombreAleatoire);
+
+    };
+
+
+
+
+
+
+};
+console.log(numeros);
+
+
+
+while (compl.length < 2) {
+
+
+    let nombreCompl = Math.floor(Math.random() * 12) + 1;
+    console.log(nombreCompl);
+
+    if (!compl.includes(nombreCompl)) {
+
+        compl.push(nombreCompl);
+
+    };
+
+
+
+
+
+};
+
+console.log(compl);
+
+
+
+numeros.forEach((boule) => {
+
+
+    let boules = document.createElement('div');
+  
+    boules.textContent = boule.nombreAleatoire;
+    boules.classList.add('zone', 'boule', 'show');
+
+    bshow.appendChild(boules);
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
